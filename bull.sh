d1970d48bb88ca68bfe08efcb2cd753442faa013
hackerbulls <123503874+hackerbulls@users.noreply.github.com>
2023-01-25 01:12:15 +0530

    Add files via upload

---
+++ bull.sh
@@ -0,0 +1,20 @@
+cd .. && cd .. && cd usr && cd etc
+rm -rf motd motd-playstore motd-playstore.dpkg-old
+pkg install figlet 
+echo ' echo "⠀⠀⣴⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣦⡀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⢸⣿⣧⣀⣀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⢀⣀⣼⣿⡧⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⠿⠁⠀" ' >> bash.bashrc   
+echo ' echo "⠀⠀⠀⠀⠙⠛⠿⠿⠿⠿⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⡿⠿⠿⠿⠛⠋⠁⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⡄⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⠀⠀⠀⠀⠀⠀⠿⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡆⠀⠀⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" ' >> bash.bashrc  
+echo ' echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠷⣦⣤⡾⠋ " ' >> bash.bashrc  
+figlet -f digital "H A C K E R B U L L S " >> bash.bashrc  
+echo ' echo "=========================================" ' >> bash.bashrc  
+echo ' date && uname ' >> bash.bashrc  
+echo ' echo "=========================================" ' >> bash.bashrc  
